acec99cf33a764ea0b704a322eb26b0acfa62206
Margherita <margherita.zanchini@studio.unibo.it>
2024-06-10 17:01:07 +0200

    managed exception in server

--- server.py
+++ server.py
@@ -6,37 +6,49 @@ import threading
 #funzione che gestisce i client che vogliono connettersi al server
 def accetta_connessioni():
     while True:
-        client_socket, client_address = SERVER_SOCKET.accept()  #l'esecuzione del programma si 
-                                            #blocca fino a quando non arriva una richiesta di connessione da un client
-        print(client_address, "si è collegato")
-        entry_message = 'ciao benvenuto sul server '+ HOST + 'inserisci il tuo nome'
-        client_socket.send(entry_message.encode('utf-8'))
-        indirizzi[client_socket] = client_address #aggiungiamo al dizionario l'indirizzo del nuovo client
-        #attiviamo il thread per quel client
-        threading.Thread(target=gestione_client, args=(client_socket,)).start()
+        try:
+            client_socket, client_address = SERVER_SOCKET.accept()  #l'esecuzione del programma si 
+                                                #blocca fino a quando non arriva una richiesta di connessione da un client
+            print(client_address, "si è collegato")
+            entry_message = 'Ciao ' + HOST + ' benvenuto sul server. Inserisci il tuo nome'
+            client_socket.send(entry_message.encode('utf-8'))
+            indirizzi[client_socket] = client_address #aggiungiamo al dizionario l'indirizzo del nuovo client
+            #attiviamo il thread per quel client
+            threading.Thread(target=gestione_client, args=(client_socket,)).start()
+        except Exception as e:
+            print("Si è verificato un errore nell'accettare una connessione. Errore: "+e)
+            break
 
 def gestione_client(client_socket):
-    data = client_socket.recv(SIZE_BUFFER) #riceviamo il nome scelto dal client
-    nome = data.decode('utf-8')
-    clients[client_socket] = nome #aggiorniamo il dizionario con il nuovo nome
-    messaggio = nome + ' si è unito!' 
-    broadcast(messaggio, '') #mandiamo a tutti il messaggio che un client si è unito
+    try:
+        nome = client_socket.recv(SIZE_BUFFER).decode('utf-8') #riceviamo il nome scelto dal client
+        clients[client_socket] = nome #aggiorniamo il dizionario con il nuovo nome
+        messaggio = nome + ' si è unito!' 
+        broadcast(messaggio, '') #mandiamo a tutti il messaggio che un client si è unito
+    except Exception as e:
+        print("Si è verificato un errore nella gestione iniziale del client. Errore: "+e)
 
     while True:
-        messaggio = client_socket.recv(SIZE_BUFFER).decode('utf-8')
-        if messaggio != '{quit}':
-            broadcast(messaggio, nome+': ')
-        else:
-            del clients[client_socket]
-            exit_message = nome + ' ha abbandonato'
-            broadcast(exit_message, '')
-            break
+        try:
+            messaggio = client_socket.recv(SIZE_BUFFER).decode('utf-8')
+            if messaggio != '{quit}':
+                broadcast(messaggio, nome+': ')
+            else:
+                del clients[client_socket]
+                exit_message = nome + ' ha abbandonato'
+                broadcast(exit_message, '')
+                break
+        except Exception as e:
+            print("Si è verificato un errore nel riceve/inviare messaggi dai/ai client. Errore: "+e)
         
 
 def broadcast(messaggio, nome):
     for client in clients:
-        new_msg = nome + '' + messaggio
-        client.send(new_msg.encode('utf-8'))
+        try:
+            new_msg = nome + '' + messaggio
+            client.send(new_msg.encode('utf-8'))
+        except Exception as e:
+            print("errore nell'inviare un messaggio in broadcast. Errore: "+e)
 
 #dizionari per la registrazione dei client e dei relativi indirizzi
 clients = {}
